2538a27ea252d09ee72f4047d32429b08c5f89fc
Gavin Estenssoro <gavin.estenssoro@pinnacol.com>
2024-01-23 21:50:50 -0700

    devcontainer files

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+mkdir -p /commandhistory
+touch /commandhistory/.zsh_history
+chown -R ${USER} /commandhistory
+
+echo "autoload -Uz add-zsh-hook; append_history() { fc -W }; add-zsh-hook precmd append_history; export HISTFILE=/commandhistory/.zsh_history" >> /home/${USER}/.zshrc
+echo "PATH=\".venv/bin:${PATH}\"" >> /home/${USER}/.zshrc
+
+pip install --upgrade pip \
+    && pip install poetry \
+    && poetry config virtualenvs.in-project true \
+    && poetry config cache-dir ./ \
+    && poetry install --with=dev --no-root
---
+++ README.md
@@ -0,0 +1,10 @@
+# dev-container
+
+Make [VSCode devcontainers](link to vscode dev containers) feel like home.
+
+This repo sets up a VSCode devcontainer with the following:
+
+* familiar shell setting via [dotfiles](link to blog)
+* [zsh](link to zsh) and [oh-my-zsh](link to oh-my-zsh)
+
+It pairs with my blog [post](link to future post) on the topic.
